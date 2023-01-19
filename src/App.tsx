f30f152d5a2465d17a96003f6808ce6468dd2b32
dtinth on node39622-dtinth.proen.app.ruk-com.cloud <dtinth@users.noreply.github.com>
2023-01-19 13:52:18 +0000

    Generate an offer

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import SimplePeer from 'simple-peer/simplepeer.min.js'
+import QRCode from 'react-qr-code'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [offer, setOffer] = useState(null as string | null)
+  useEffect(() => {
+    const peer = new SimplePeer({
+      initiator: true,
+      trickle: false,
+    })
+
+    peer.on('signal', data => {
+      setOffer(JSON.stringify(data))
+    })
+
+    return () => {
+      peer.destroy()
+    }
+  }, [])
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {offer && <QRCode value={offer} />}
     </div>
   )
 }
---
+++ src/typings.d.ts
@@ -0,0 +1,5 @@
+declare module 'simple-peer/simplepeer.min.js' {
+    export * from 'simple-peer';
+    import SimplePeer from 'simple-peer';
+    export default SimplePeer;
+}
